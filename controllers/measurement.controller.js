37dd4db9e648c5df3cef7f335271dc1352f52bfc
Henry Mackay <hmm.mackay@gmail.com>
2024-03-27 17:18:27 -0400

    date range functionality

--- controllers/measurement.controller.js
+++ controllers/measurement.controller.js
@@ -1,38 +1,55 @@
-const MeasModel = require('../models/measurement.model');
-var fs = require('fs'); // new
+const MeasModel = require('../models/measurement.model.js');
+const PresetModel = require('../models/preset.model.js');
 
-
-exports.insert = (req, res) => {
-
-var outbox = JSON.parse(fs.readFileSync('./public/outbox.json', 'utf8')); // file containing user input on what to set actuators
-
+exports.set = (req, res) => {
   MeasModel.newMeas(req.body)
     .then((result) => {
-      const latestMeasurement = {
-        actuator1Set: outbox.actuator1Set, //upon measurement being posted, respond with command set
-        actuator2Set: outbox.actuator2Set,
-        actuator3Set: outbox.actuator3Set,
-	actuator4Set: outbox.actuator4Set,
-	actuator5Set: outbox.actuator5Set
-      };
-      res.setHeader('Content-Type', 'application/json'); //set headers for result and format
-      res.end(JSON.stringify(latestMeasurement,null,3));
+      // Retrieve the current preset data (which includes override data)
+      return PresetModel.get();
+    })
+    .then((preset) => {
+      if (preset) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(preset, null, 3));
+      } else {
+        res.status(404).send('Preset data not found');
+      }
+    })
+    .catch((error) => {
+      console.error('Error inserting measurement:', error);
+      res.status(500).send('Internal Server Error');
     });
 };
 
+exports.get = (req, res) => {
+  let limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
+  let page = 0;
 
-
-exports.list = (req, res) => {
-    let limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
-    let page = 0;
-    if (req.query) {
-        if (req.query.page) {
-            req.query.page = parseInt(req.query.page);
-            page = Number.isInteger(req.query.page) ? req.query.page : 0;
-        }
+  if (req.query) {
+    if (req.query.page) {
+      req.query.page = parseInt(req.query.page);
+      page = Number.isInteger(req.query.page) ? req.query.page : 0;
     }
-    MeasModel.list(limit, page)
-        .then((result) => {
-            res.status(200).send(result);
-        })
+  }
+
+  const from = req.query.from;
+  const to = req.query.to;
+
+  const query = {};
+  if (from && to) {
+    query.timestamp = { $gte: new Date(from), $lte: new Date(to) };
+  } else if (from) {
+    query.timestamp = { $gte: new Date(from) };
+  } else if (to) {
+    query.timestamp = { $lte: new Date(to) };
+  }
+
+  MeasModel.list(query, limit, page)
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((error) => {
+      console.error('Error retrieving measurements:', error);
+      res.status(500).send('Internal Server Error');
+    });
 };
